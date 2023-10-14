bf6a45abab7561c034fdadc6faf97add7877d13a
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-14 04:56:15 +0200

    Creating JPA Entity - Post Entity

---
+++ src/main/java/com/dgmf/entity/Post.java
@@ -0,0 +1,38 @@
+package com.dgmf.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Entity
+@Table(
+        name = "posts",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"title"})
+        }
+)
+public class Post {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false, unique = true)
+    private String content;
+    @Column(name = "creation_date", nullable = false)
+    // Hibernate will automatically take the current Timestamp of the JVM
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+    @Column(name = "last_update")
+    // Hibernate will automatically take the current Timestamp of the JVM
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
+}
