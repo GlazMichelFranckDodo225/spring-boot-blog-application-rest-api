001f7308cc7f26a1cf22f531b14db4536c984d04
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-17 05:10:26 +0200

    Creating DTOs Classes - PostDtoRequest and PostDtoResponse

---
+++ src/main/java/com/dgmf/dto/PostDtoRequest.java
@@ -0,0 +1,13 @@
+package com.dgmf.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class PostDtoRequest {
+    private String title;
+    private String description;
+    private String content;
+}
