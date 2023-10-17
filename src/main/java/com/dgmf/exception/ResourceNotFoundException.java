c6b8164002a1f9490c25be2d70db72284d4fdaac
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-17 04:58:06 +0200

    Creating Custom Exception - ResourceNotFoundException

---
+++ src/main/java/com/dgmf/exception/ResourceNotFoundException.java
@@ -0,0 +1,33 @@
+package com.dgmf.exception;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Data
+// This Annotation causes Spring Boot to respond with the
+// specified HTTP Status Code whenever this Exception is
+// thrown from the Controller
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private String fieldValue;
+
+    public ResourceNotFoundException(
+            String resourceName,
+            String fieldName,
+            String fieldValue
+    ) {
+        // Post Not Found with id : 1
+        super(String.format("%s Not Found with %s : '%s'",
+                resourceName,
+                fieldName,
+                fieldValue)
+        );
+
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
