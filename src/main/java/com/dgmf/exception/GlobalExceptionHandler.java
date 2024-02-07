c274d6e7c8f53ed0e87ca89ccc88f1cd8f3d35a3
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-07 05:58:41 +0100

    Specific and Custom Exceptions Handling

---
+++ src/main/java/com/dgmf/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.dgmf.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+// Class Responsible to Handle Specific (ResourceNotFoundException, BlogAPIException)
+// and Global Exceptions
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    // Specific "ResourceNotFoundException" Exception
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
+            ResourceNotFoundException resourceNotFoundException,
+            WebRequest webRequest // To Send Back Details to the Client
+    ) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .timestamp(new Date())
+                .message(resourceNotFoundException.getMessage())
+                // False ==> To Send Back Only the URI instead of the
+                // Full Description
+                .details(webRequest.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    // Specific "BlogAPIException" Exception
+    @ExceptionHandler(BlogAPIException.class)
+    public ResponseEntity<ErrorDetails> handleBlogAPIException(
+            BlogAPIException blogAPIException,
+            WebRequest webRequest // To Send Back Details to the Client
+    ) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .timestamp(new Date())
+                .message(blogAPIException.getMessage())
+                // False ==> To Send Back Only the URI instead of the
+                // Full Description
+                .details(webRequest.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+}
