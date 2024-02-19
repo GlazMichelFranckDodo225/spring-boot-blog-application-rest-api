5eb810a6b6a3cc3e2430eaa5d436a4115ef4efc0
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-19 07:31:23 +0100

    Customizing Validation Response

--- src/main/java/com/dgmf/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dgmf/exception/GlobalExceptionHandler.java
@@ -1,17 +1,24 @@
 package com.dgmf.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 // Class Responsible to Handle Specific (ResourceNotFoundException, BlogAPIException)
 // and Global Exceptions
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     // Specific "ResourceNotFoundException" Exception
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
@@ -64,4 +71,41 @@ public class GlobalExceptionHandler {
                 errorDetails, HttpStatus.INTERNAL_SERVER_ERROR
         );
     }
+
+    // First Approach ==> Overriding
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    // Second Approach ==> Specific Exception
+    // Specific "MethodArgumentNotValidException ex" Exception
+    /*@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException methodArgumentNotValidException,
+            WebRequest webRequest // To Send Back Details to the Client
+    ) {
+        Map<String, String> errors = new HashMap<>();
+
+        methodArgumentNotValidException.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }*/
 }
