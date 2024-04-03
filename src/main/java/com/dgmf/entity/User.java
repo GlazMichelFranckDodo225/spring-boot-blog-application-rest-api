f28ab3c7fd5b2d6102fa0fc52b995d88d7eceb8b
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-03 17:18:33 +0200

    Creation of User and Role Entities

---
+++ src/main/java/com/dgmf/entity/User.java
@@ -0,0 +1,22 @@
+package com.dgmf.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
