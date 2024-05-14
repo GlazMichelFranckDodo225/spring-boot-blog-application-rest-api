5233e989f748fc591aff5567f16fe6f0f4adf6ea
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-14 18:36:41 +0200

    CSRF Branch - Error

--- src/main/java/com/dgmf/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dgmf.repository;
-
-import com.dgmf.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
