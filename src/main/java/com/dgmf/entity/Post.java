73856992ab1415ea535452a73e8ea2dab3bcb0dd
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-26 05:57:20 +0200

    Unidirectional OneToMany Relationship Between Category and Post Entities

--- src/main/java/com/dgmf/entity/Post.java
+++ src/main/java/com/dgmf/entity/Post.java
@@ -13,26 +13,31 @@ import java.util.Set;
 
 @Entity
 @Table(
-        name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+        name = "posts",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"title"})
+        }
 )
 public class Post {
-
-    @Id
-    @GeneratedValue(
-            strategy = GenerationType.IDENTITY
-    )
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(name = "title", nullable = false)
     private String title;
-
     @Column(name = "description", nullable = false)
     private String description;
-
     @Column(name = "content", nullable = false)
     private String content;
-
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(
+            mappedBy = "post",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
     private Set<Comment> comments = new HashSet<>();
-
+    // OneToMany Bidirectional Relationship Between Post and
+    // Category Entities (Parent ==> Category / Post ==> Child)
+    @ManyToOne(fetch = FetchType.LAZY) // Fetch Category on Demand (Not
+    // immediately) by Calling the Related Getter
+    // To Specify the Foreign Key Name
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
