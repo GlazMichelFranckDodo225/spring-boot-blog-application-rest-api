c866be17d6f8f36d3655535042ad2ab2ad56c66e
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-03 09:06:56 +0200

    UserDetailsService Interface Implementation - CustomUserDetailsService

--- src/main/java/com/dgmf/security/CustomUserDetailsService.java
+++ src/main/java/com/dgmf/security/CustomUserDetailsService.java
@@ -8,13 +8,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.Set;
 import java.util.stream.Collectors;
 
+// Spring Security will Use this Class to Load User from DB and
+// Perform Database Authentication
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-
     private UserRepository userRepository;
 
     public CustomUserDetailsService(UserRepository userRepository) {
@@ -22,18 +22,35 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
-          User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
-                 .orElseThrow(() ->
-                         new UsernameNotFoundException("User not found with username or email: "+ usernameOrEmail));
+    public UserDetails loadUserByUsername(String usernameOrEmail)
+            throws UsernameNotFoundException
+    {
+          // Load User from DB
+          User user = userRepository.findByUsernameOrEmail(
+                  usernameOrEmail, usernameOrEmail
+                  )
+                 .orElseThrow(
+                         () -> new UsernameNotFoundException(
+                                 "User not found with username or email: " +
+                                         usernameOrEmail
+                                )
+                 );
 
-        Set<GrantedAuthority> authorities = user
+
+          // Convert a Set of Roles into a Set of Granted
+          // Authorities Regarding Spring Security Expectations
+          Set<GrantedAuthority> authorities = user
                 .getRoles()
                 .stream()
-                .map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                  .collect(Collectors.toSet());
 
-        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+        // Convert Retrieved User from DB to Spring
+        // Security Provided User Object and Return It
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
                 user.getPassword(),
-                authorities);
+                authorities
+        );
     }
 }
