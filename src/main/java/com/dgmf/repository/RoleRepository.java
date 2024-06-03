f774af3c635d8733cdad8f341960588aa5cc2b66
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-03 08:42:13 +0200

    Create UserRepository and RoleRepository Interfaces

--- src/main/java/com/dgmf/repository/RoleRepository.java
+++ src/main/java/com/dgmf/repository/RoleRepository.java
@@ -2,7 +2,6 @@ package com.dgmf.repository;
 
 import com.dgmf.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
